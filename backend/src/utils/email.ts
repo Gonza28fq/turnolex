7cbd198f1a5fa3760ae09bfcb89847a4f6b4b23b
TuNombre <tuemail@example.com>
2026-02-28 19:49:47 -0300

    fix: nodemailer tls config

--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,15 +1,15 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
-
 interface EmailTurno {
   clienteNombre: string;
   clienteEmail: string;
